db1394d1933ab443a89c49a34751506ada58852c
Zelda Hessler <zelda.hessler@pm.me>
2022-01-29 16:35:18 -0600

    add: TODO lightbox

--- components/Gallery/Lightbox.js
+++ components/Gallery/Lightbox.js
@@ -1,3 +1,8 @@
-export default function Lightbox({ children }) {
-  return <div>Gallery</div>;
+function photoPath(root, filename) {
+  return `${root}/${filename}.webp`;
+}
+
+// TODO figure out how to add an accessible modal
+export default function Lightbox({ photo }) {
+  return <div>Lightbox</div>;
 }
